dedd02d1d0ecc07ce3461e73490d3a671a9a1f72
moshood <mushman360@gmail.com>
2023-08-13 15:04:48 +0100

    added tests for engines component and batteries

--- test/test_car.py
+++ test/test_car.py
@@ -1,8 +1,74 @@
 import unittest
 from datetime import datetime
+from battery_dir.battery import Battery
 
 
 from car_factory import CarFactory
+
+from engine_dir.engines import CapuletEngine,WilloughbyEngine,SternmanEngine
+from battery_dir.batteries import Nubbin, Spindler
+
+
+
+
+class TestCapuletEngine(unittest.TestCase):
+    def test_should_be_serviced(self):
+        current_mileage = 30001
+        last_service_mileage = 0
+        engine = CapuletEngine(last_service_mileage,current_mileage)
+        self.assertTrue(engine.needs_service())
+
+    def test_should_not_be_serviced(self):
+        current_mileage = 30000
+        last_service_mileage = 0
+        engine = CapuletEngine(last_service_mileage,current_mileage)
+        self.assertFalse(engine.needs_service())
+
+class TestWilloughbyEngine(unittest.TestCase):
+    def test_should_be_serviced(self):
+        current_mileage = 60001
+        last_service_mileage = 0
+        engine = WilloughbyEngine(last_service_mileage,current_mileage)
+        self.assertTrue(engine.needs_service())
+    def test_should_not_be_serviced(self):
+        current_mileage = 60000
+        last_service_mileage = 0
+        engine = WilloughbyEngine(last_service_mileage,current_mileage)
+        self.assertFalse(engine.needs_service())
+
+class TestSternmanEngine(unittest.TestCase):
+    def test_should_be_serviced(self):
+        warning_light_is_on = True
+        engine = SternmanEngine(warning_light_is_on)
+        self.assertTrue(engine.needs_service())
+    def test_should_not_be_serviced(self):
+        warning_light_is_on = False
+        engine = SternmanEngine(warning_light_is_on)
+        self.assertFalse(engine.needs_service())
+# Nubbin, Spindler
+class TestNubbin(unittest.TestCase):
+    def test_should_be_serviced(self):
+        today = datetime.today().date()
+        last_service_date = today.replace(year=today.year - 5)
+        battery = Nubbin(last_service_date,today)
+        self.assertTrue(battery.needs_service())
+    def test_should_not_be_serviced(self):
+        today = datetime.today().date()
+        last_service_date = today.replace(year=today.year - 3)
+        battery = Nubbin(last_service_date,today)
+        self.assertFalse(battery.needs_service())
+
+class TestSpindler(unittest.TestCase):
+    def test_should_be_serviced(self):
+        today = datetime.today().date()
+        last_service_date = today.replace(year=today.year - 3)
+        battery = Spindler(last_service_date,today)
+        self.assertTrue(battery.needs_service())
+    def test_should_not_be_serviced(self):
+        today = datetime.today().date()
+        last_service_date = today.replace(year=today.year - 1)
+        battery = Spindler(last_service_date,today)
+        self.assertFalse(battery.needs_service())
 
 # today = datetime.today().date()
 # last_service_date = today.replace(year=today.year - 3)
